57eab509b7be4a35d3d8c9668d8ba8df1703c7c7
Toben Green <tobensg@gmail.com>
2024-08-08 22:52:03 -0700

    a solution to the two sorted merges

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,7 +2,31 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+const getLogAsync = async (logSource, index) => {
+  return logSource.popAsync().then(value => {return value ? {...value, index, nextLog: logSource.popAsync()} : value});
+}
+
 module.exports = (logSources, printer) => {
+  const sourcePromises = logSources.map(
+    (source, index) => {
+      return getLogAsync(source, index);
+    }
+  )
+  Promise.all(sourcePromises).then(async logArray => {
+    logArray.sort((a, b) => b.date - a.date);
+    while (logArray.length) {
+      const nextLog = logArray.pop();
+      printer.print(nextLog);
+      const logIndex = nextLog.index;
+      const secondLog = await nextLog.nextLog
+      if (secondLog) {
+        const newLog = {...secondLog, index: logIndex, nextLog: logSources[logIndex].popAsync()}
+        logArray.push(newLog);
+        logArray.sort((a, b) => b.date - a.date);
+      }
+    }
+    printer.done()
+  });
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,33 @@
 "use strict";
+const getLog = (logSource, index) => {
+  const log = logSource.pop();
+  // this allows us to pass either the log object with an included 
+  // index or just the false value
+  return log ? {...log, index} : log;
+};
 
+const fetchAndSortLogs = (logSources) => {
+  const logs = logSources.reduce((acc, curr, ind) => {
+    const fetchedLog = getLog(curr, ind)
+    const logs = fetchedLog ? [...acc, fetchedLog] : acc;
+    return logs;
+  }, []);
+  return logs.sort((a, b) => b.date - a.date);
+};
 // Print all entries, across all of the sources, in chronological order.
-
 module.exports = (logSources, printer) => {
+  // syncLogSources has n sources.  call pop to get data
+  // data object contains date and msg
+  const sortedLogs = fetchAndSortLogs(logSources);
+  while (sortedLogs.length) {
+    const nextLog = sortedLogs.pop();
+    printer.print(nextLog);
+    const fetchedLog = getLog(logSources[nextLog.index], nextLog.index)
+    if (fetchedLog) {
+      sortedLogs.push(fetchedLog)
+      sortedLogs.sort((a, b) => b.date - a.date);
+    }
+  }
+  printer.done()
   return console.log("Sync sort complete.");
 };
